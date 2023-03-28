55676e332fbdfa151df40644f2e24160ab54bf87
meme escape <memescap3@gmail.com>
2023-03-28 22:19:11 +0000

    assignment done

--- server.js
+++ server.js
@@ -1,74 +1,155 @@
-// console.log("hello")
 
 // const http = require("http")
+// const Users = [
+// {
+// name: "Bruce",
+// age: 25
+// },
+// {
+// name: "Tony",
+// age: 30
+// },
+// {
+// name: "Steve",
+// age: 70
+// }
+// ]
+// const server = http.createServer(function (request, response) {
+// const paths = request.url.split("/")
 
-// const Users = [{
-//     name:"something",age:46
-// }]
+// console.log("---methods--- ", request.method)
 
-// const server = http.createServer(function(request, response){
+// if(request.method === "GET" && paths[1] === "users" && paths.length === 2) {
+// response.write(JSON.stringify(Users))
+// }
+// else if(request.method === "GET" && paths[1] === "users" && paths[2]) {
+// const idx = paths[2]
+// const user = Users[idx]
+// if(user) {
+// response.write(JSON.stringify(user))
+// }
+// }
+// else if(request.method === "POST" && paths[1] === "users"){
+//   // response.statusCode(201)
+//   // response.write("User Data Created")
 
-//     console.log(request.url)
-//     response.write("hello Biro!")
-//     // response.write(JSON.stringify(Users))
+//   let data = ""
 
-//     response.end()
+//   request.on("data", function (chunk){
+//     data += chunk
+//   })
+
+//   request.on("end", function(){
+//     const obj = JSON.parse(data.toString())
+//     Users.push(obj)
+//   })
+// }
+// else if(request.method === "PUT" && paths[1] === "users" && paths[2]){
+//     const idx = paths[2]
+//     let data = ""
+//     request.on("data", function (chunk) {
+//       data += chunk
+      
+// })
+// request.on("end", function () {
+//     const obj = JSON.parse(data.toString())
+//     Users[idx] = obj
+//     // Users.push(obj)
+    
+//   })
+// }
+// else if(request.method === "DELETE" && paths[1] === "users" && paths[2]){
+//     const idx = paths[2]
+//     Users.splice(idx, 1)
+// }
+// else {
+// response.write("Not Found")
+// }
+// response.end()
+// })
+// server.listen(3000, function () {
+// console.log("server is running on port number 3000")
 // })
 
-// server.listen(3000, function(){
-//     console.log("server is running at 3000")
-// })
 
-const http = require("http")
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////  converted this code to arrow function ////////////////////////////////////////////////////////////////////////////
+
+const http = require("http");
+
 const Users = [
-{
-name: "Bruce",
-age: 25
-},
-{
-name: "Tony",
-age: 30
-},
-{
-name: "Steve",
-age: 70
-}
-]
-const server = http.createServer(function (request, response) {
-const paths = request.url.split("/")
+  {
+    name: "Bruce",
+    age: 25,
+  },
+  {
+    name: "Tony",
+    age: 30,
+  },
+  {
+    name: "Steve",
+    age: 70,
+  },
+];
 
-console.log("---methods--- ", request.method)
+const server = http.createServer((request, response) => {
+  const paths = request.url.split("/");
 
-if(request.method === "GET" && paths[1] === "users" && paths.length === 2) {
-response.write(JSON.stringify(Users))
-}
-else if(request.method === "GET" && paths[1] === "users" && paths[2]) {
-const idx = paths[2]
-const user = Users[idx]
-if(user) {
-response.write(JSON.stringify(user))
-}
-}
-else if(request.method === "POST" && paths[1] === "users"){
-  // response.statusCode(201)
-  // response.write("User Data Created")
+  console.log("---methods--- ", request.method);
 
-  let data = ""
+  if (request.method === "GET" && paths[1] === "users" && paths.length === 2) {
+    response.write(JSON.stringify(Users));
+  } else if (
+    request.method === "GET" && paths[1] === "users" && paths[2]
+  ) {
+    const idx = paths[2];
+    const user = Users[idx];
+    if (user) {
+      response.write(JSON.stringify(user));
+    }
+  } else if (request.method === "POST" && paths[1] === "users") {
+    let data = "";
 
-  request.on("data", function (chunk){
-    data += chunk
-  })
+    request.on("data", (chunk) => {
+      data += chunk;
+    });
 
-  request.on("end", function(){
-    const obj = JSON.parse(data.toString())
-    Users.push(obj)
-  })
-}
-else {
-response.write("Not Found")
-}
-response.end()
-})
-server.listen(3000, function () {
-console.log("server is running on port number 3000")
-})
+    request.on("end", () => {
+      const obj = JSON.parse(data.toString());
+      Users.push(obj);
+    });
+  } else if (request.method === "PUT" && paths[1] === "users" && paths[2]) {
+    const idx = paths[2];
+    let data = "";
+
+    request.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    request.on("end", () => {
+      const obj = JSON.parse(data.toString());
+      Users[idx] = obj;
+    });
+  } else if (
+    request.method === "DELETE" && paths[1] === "users" && paths[2]
+  ) {
+    const idx = paths[2];
+    Users.splice(idx, 1);
+  } else {
+    response.write("Not Found");
+  }
+
+  response.end();
+});
+
+server.listen(3000, () => {
+  console.log("server is running on port number 3000");
+});
